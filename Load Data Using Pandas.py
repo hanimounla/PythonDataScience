e90c71cdfa3e93b9ad464a8f7b77b42395d6924d
Hani Mounla <hani-mounla@live.com>
2017-09-23 12:02:21 +0300

    Added load data using pandas file

---
+++ Load Data Using Pandas.py
@@ -0,0 +1,5 @@
+#the most used python package to deal with data is called pandas we load it using
+import pandas as pd #and usually name it "pd"
+
+df = pd.read_csv("wheatData.data") #create a datafram and assign the wheat data to it
+print df #view the data
