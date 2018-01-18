cd2324b35f9935cf0d8b135f2389f4ae0583f93d
Hani Mounla <hani-mounla@live.com>
2018-01-18 23:17:23 +0200

    Updates on plotting.py file

--- Codes/Plotting.py
+++ Codes/Plotting.py
@@ -1,10 +1,8 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #this library is the most used for data plotting and usualy call it plt
 
-x_range = range(10)
-y_range = range(10)
-plt.yticks(x_range)
-plt.xticks(y_range)
-plt.plot([1,2,3],[4,6,5], color = 'b', marker = 'o', label = 'line1')
-plt.plot([1,2,3],[5,3,2], color = 'r', marker = '*', label = 'line2')
-plt.legend()
-plt.show()
+
+plt.plot([1,2,3],[4,5,3], color = 'b', marker = 'o', label = 'line1') #first line (1,4),(2,5),(3,3)
+plt.plot([1,2,3],[5,3,4], color = 'r', marker = '*', label = 'line2') #second line (1,5),(2,3),(3,4)
+
+plt.legend() #to display the legend for the chart
+plt.show() # show the chart
