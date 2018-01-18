549f3efcb2b4eae8fbc5d25e3b3a1c27341f8232
Hani Mounla <hani-mounla@live.com>
2018-01-18 20:46:46 +0200

    some adds about pandas

--- Codes/Load Data Using Pandas.py
+++ Codes/Load Data Using Pandas.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("DataSets\wheatData.data")  # create a data frame and assign the wheat data to it
 # as a beginning its always great to check the data first, here are some examples :
-print df  # view the whole data
-print df.head(5)  # view first 5 records
-print df.tail(5)  # view last 5 records
+df  # view the whole data
+df.head(5)  # view first 5 records
+df.tail(5)  # view last 5 records
 
-print df.describe  # view statistics about the data frame(min, max, standard deviation,...)
+#Statistics about the dataFrame
+df.describe  # view statistics about the data frame(min, max, standard deviation,...)
 df.hist() # creates multiple plots correspnds with the number of featues in the dataframe (df)
+
+#Slicing and dicing dataframe
+df.loc[[1],:] #show first row of the dataframe
+df.loc[:,[1,2]] # show columns 1 & 2
+
