70532a86eea806be2e7011735b84fc13988c215e
Hani Mounla <hani-mounla@live.com>
2018-01-18 23:03:45 +0200

    Added some plotting examples

--- Codes/Plotting.py
+++ Codes/Plotting.py
@@ -1,1 +1,10 @@
 import matplotlib.pyplot as plt
+
+x_range = range(10)
+y_range = range(10)
+plt.yticks(x_range)
+plt.xticks(y_range)
+plt.plot([1,2,3],[4,6,5], color = 'b', marker = 'o', label = 'line1')
+plt.plot([1,2,3],[5,3,2], color = 'r', marker = '*', label = 'line2')
+plt.legend()
+plt.show()
